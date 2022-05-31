a5b3a9815dcabefba56233dd2ce90c14c48a84c7
Jemmy-cloud <20172532@fue.edu.eg>
2022-05-31 17:00:14 -0400

    Second commit

---
+++ src/com/cognixia/jump/login/ConnectionManager.java
@@ -0,0 +1,46 @@
+package com.cognixia.jump.login;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+	
+	
+
+	private static Connection connection = null;
+	
+	private static final String URL = "jdbc:mysql://localhost:3306/ptp";
+	// ?serverTimezone=EST5EDT <-- add to end of URL if on Mac or Linux
+	private static final String USERNAME = "root";
+	private static final String PASSWORD = "root"; // change password if need be,
+												   // might be Root@123 for mac users
+	
+	private static void makeConnection() {
+
+		try {
+			connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+		} catch (SQLException e) {
+			System.out.println("Could not connect to database");
+		}
+
+	}
+
+	
+	
+	
+	public static Connection getConnection() {
+
+		if (connection == null) {
+			makeConnection();
+		}
+
+		return connection;
+	}
+	
+	
+	
+
+
+}
--- src/com/cognixia/jump/login/Login.java
+++ src/com/cognixia/jump/login/Login.java
@@ -1,5 +1,6 @@
 package com.cognixia.jump.login;
 
+import java.sql.Connection;
 import java.util.InputMismatchException;
 
 
@@ -80,42 +81,166 @@ public class Login {
 				
 
 
-				    String Username;
+				    String userid;
 				    String Password;
 
 				    Password = "123";
-				    Username = "pip";
-
+				    userid = "pip";
+				    //email = " Jemmy_ahmed@live.com ";
+					
 				    Scanner input1 = new Scanner(System.in);
+				    Connection conn = ConnectionManager.getConnection();
+		//	Statement st = conn.createStatement("INSERT into users ");
+			
 			
 				    System.out.println("Please Enter Username : ");
 				    String username = input1.next();
-
+				
+		    	
 				    Scanner input2 = new Scanner(System.in);
 				    System.out.println("Please Enter Password : ");
 				    String password = input2.next();
-
-				    if (username.equals(Username) && password.equals(Password)) {
+					
+				  //  st.executeUpdate("INSERT")
+					
+				    if (username.equals(userid) && password.equals(Password)) {
 
 				        System.out.println("Access Granted! Welcome!");
+				        
+				        ShowsMenu();				        
 				    }
 
-				    else if (username.equals(Username)) {
+				    else if (username.equals(userid)) {
 				        System.out.println("Invalid Password!");
 				    } else if (password.equals(Password)) {
 				        System.out.println("Invalid Username!");
 				    } else {
 				        System.out.println("Invalid Username & Password!");
 				    }
+				    
 			
 			}
+			
+			
+		
+				    
+				    
+				    
+				    
+				    
+				    
+		finally {
+		    // ... cleanup that will execute whether or not an error occurred ...
+		}	  
+			
+			
 
-			finally {
-			    // ... cleanup that will execute whether or not an error occurred ...
-			}	
+			
 	
 		}}
+	
+	
+	
+	public static void ShowsMenu() {
+	    
+		
+		while(true) {
 			
+			try {
+			
+				System.out.println("Welcome Back!" + 
+				
+							"\n1. add shows planning to watch" +
+						    "\n2. Select currently watching shows" +
+						    "\n3. Show finished watching categories" +
+							"\n4. Exit to return to main menu");
+				
+				int option = sc.nextInt();
+				sc.nextLine();
+				
+				switch (option) {
+				case 1:
+					addShows();
+					break;
+				case 2:
+					watchShows();
+					break;
+				case 3:
+					
+					break;
+				default:
+					System.out.println("Enter number between 1 and 3");
+					break;
+				}
+				
+				if(option == 3) {
+					break;
+				}
+				
+				
+			} catch(InputMismatchException e) {
+				sc.nextLine();
+				System.out.println("Enter number between 1 and 3");
+			}
+		}
+		
+	}
+
+
+	public static void addShows() {
+		
+	
+		 Scanner input = new Scanner(System.in);
+		 String decision;
+		 Scanner kbd = new Scanner (System.in);
+		System.out.println("Enter your Favourite Shows : ");
+		 String fav = input.next();
+		
+		   System.out.println("Added " +fav + " Successfully!");
+		   System.out.println("Add Another Show ?");
+		   decision = kbd.nextLine();
+
+	        switch(decision){
+	   
+	        case "yes":
+	       
+	        	System.out.println("Enter your Favourite Shows : ");
+	        	input.next();
+	            break;
+	        	  
+	        case "no": 
+	        	System.out.println("  : ");
+	            break;
+
+     
+		        
+		            
+		    }
+		
+	}
+	
+	
+			
+	
+	public static void watchShows() {
+		
+		
+		 Scanner input = new Scanner(System.in);
+		 Scanner kbd = new Scanner (System.in);
+		System.out.println("Enter your Favourite Show : ");
+		 String fav = input.next();
+		
+		   System.out.println("Added " +fav + " Successfully!");
+		   System.out.println("Add Another Show ?");
+	
+
+
+
+
+	}
+	
+	
+	
 			
 		
 	
@@ -175,8 +300,9 @@ while(true) {
 				    if (username.equals(Email) && password.equals(Password)) {
 
 				        System.out.println("Access Granted! Welcome!");
+				        
+				    		
 				    }
-
 				    else if (username.equals(Email)) {
 				        System.out.println("Invalid Password!");
 				    } else if (password.equals(Password)) {
@@ -186,6 +312,7 @@ while(true) {
 				    }
 
 				
+				   
 				
 			}finally {
 			    // ... cleanup that will execute whether or not an error occurred ...
