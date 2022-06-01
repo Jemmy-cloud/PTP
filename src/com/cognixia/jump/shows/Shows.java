b33ef1f37180aced0c38ea2268a7ffdf3f52d54a
Chris <cms5377@uncw.edu>
2022-06-01 10:47:42 -0400

    able to show shows by id

--- src/com/cognixia/jump/shows/Shows.java
+++ src/com/cognixia/jump/shows/Shows.java
@@ -4,20 +4,70 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 import com.cognixia.jump.JDBC.ConnectionManager;
 
 public class Shows {
 	
+	private static Scanner sc = new Scanner(System.in);
+	private static Connection conn = ConnectionManager.getConnection();
+	
 	public static void main(String[] args) {
 		
-		Connection conn = ConnectionManager.getConnection();
+		menu(conn, sc);
+		
+		
+	}
+
+	private static void menu(Connection conn, Scanner sc) {
+		System.out.println("Please choose one of the following options: \n"
+							+"1) Show all shows \n" 
+							+"2) Show shows by Id");
+		int option = sc.nextInt();
+		switch(option) {
+			case 1:
+				showShows(conn);
+			case 2:
+				showsByID(conn);
+		}
+		
+	}
+
+	
+	private static void showsByID(Connection conn) {
+		try {
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM watched JOIN shows ON watched.showid = shows.showid");
+			
+			System.out.println("\nHere are all the shows that you have watched \n");
+			
+			while(rs.next()) {
+				System.out.printf("%-20s", rs.getString("showname"));
+				System.out.printf("%-10s", "You have watched " + rs.getInt("progress") + " Episodes");
+				System.out.printf("%-10s", "  out of " + rs.getInt("episodes"));
+				
+				double progress = rs.getInt("progress");
+				double episodes = rs.getInt("episodes");
+				int percent = (int)( (progress / episodes) * 100) ;
+				
+				System.out.printf("%-10s %n", " you are " + percent + "% done!");
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	private static void showShows(Connection conn) {
 		
 		try {
 			Statement stmt = conn.createStatement();
 			ResultSet rs = stmt.executeQuery("SELECT showname, seasons, episodes FROM shows");
 			
-			System.out.println("Here are all the shows to choose from: \n");
+			System.out.println("\nHere are all the shows to choose from: \n");
+			
 			while(rs.next()) {
 				System.out.printf("%-20s", rs.getString("showname"));
 				System.out.printf("%-10s", "Number of seasons: " + rs.getInt("seasons"));
@@ -25,7 +75,6 @@ public class Shows {
 			}
 			
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
