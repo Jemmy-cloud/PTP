385ce7d2b15282eb035079178d5924b486a492d7
anfalfadhil <anfalfadhil90@gmail.com>
2022-06-02 12:27:03 -0400

    progress showing is done

---
+++ src/com/cognixia/jump/progress/Progress.java
@@ -0,0 +1,93 @@
+package com.cognixia.jump.progress;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.cognixia.jump.JDBC.ConnectionManager;
+import com.cognixia.jump.JDBC.dao.Show;
+import com.cognixia.jump.JDBC.dao.ShowDAOClass;
+import com.cognixia.jump.JDBC.dao.Watched;
+import com.cognixia.jump.JDBC.dao.WatchedDAOClass;
+
+public class Progress {
+	
+	
+	
+	
+	
+	
+	
+public static List<Watched> showInprogress() {
+		
+		List<Watched> inprogress = new ArrayList<Watched>();
+		
+		WatchedDAOClass watchedDAO = new WatchedDAOClass();
+		
+		for (Watched watched : watchedDAO.getAllWatchedShows()) {
+			if (watched.getProgress() != 0 )
+			inprogress.add(watched);
+		
+		}
+		
+		return inprogress;
+		
+	}
+
+
+
+
+	public static List<Show> notStartedShows() {
+		
+		List<Show> notStarted = new ArrayList<Show>();
+		
+		
+		Connection conn = ConnectionManager.getConnection();		
+		ShowDAOClass show = new ShowDAOClass();
+		List<Show> allShows = show.getAllShows();
+		WatchedDAOClass watched = new WatchedDAOClass();
+		List<Watched> inprogressShows = watched.getAllWatchedShows(); 
+		
+		
+		for (Show show1 : allShows) {
+			for (Watched watched1 : inprogressShows) {
+				
+				if (show1.getShowid() == watched1.getShowId() & watched1.getProgress() == 0 ) {
+					notStarted.add(show1);
+				}
+			}
+		}
+		
+
+		
+		return notStarted;
+		
+	}
+	
+	
+	public static List<Show> completedShows() {
+		
+		
+		List<Show> completedShows = new ArrayList<Show>();
+		Connection conn = ConnectionManager.getConnection();		
+		ShowDAOClass show = new ShowDAOClass();
+		List<Show> allShows = show.getAllShows();
+		
+		WatchedDAOClass watched = new WatchedDAOClass();
+		List<Watched> inprogressShows = watched.getAllWatchedShows(); 
+		
+		
+		for (Show show1 : allShows) {
+			for (Watched watched1 : inprogressShows) {
+				if (show1.getShowid() != watched1.getShowId() & watched1.getProgress() == show1.getEpisodes()) {
+					completedShows.add(show1);
+				}
+			}
+		}
+		
+		
+		return completedShows;
+		
+	}
+
+}
